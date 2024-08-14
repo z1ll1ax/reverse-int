5bc8faa5cb881f6c527cc1b582929f6028b3e095
Vlad Semenenko <vlad_semenenko_2016@mail.ru>
2024-08-14 18:26:10 +0300

    feat: works

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  let ans = 0;
+  n = Math.abs(n);
+    while (!(n < 10)){
+        ans *= 10;
+        ans += (n % 10);
+        n/= 10;
+        n = Math.floor(n);
+    }
+    ans *= 10;
+    ans += (n % 10);
+    return ans;
 }
